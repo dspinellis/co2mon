16dc4970db66d89d999e802d7058b794f0952cd4
Diomidis Spinellis <dds@aueb.gr>
2023-11-04 14:41:12 +0200

    Convert to Python 3
    While at it, remove trailing spaces

--- monitor.py
+++ monitor.py
@@ -8,27 +8,27 @@ import sys, fcntl, time, os, argparse
 def decrypt(key,  data):
     cstate = [0x48,  0x74,  0x65,  0x6D,  0x70,  0x39,  0x39,  0x65]
     shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
-    
+
     phase1 = [0] * 8
     for i, o in enumerate(shuffle):
         phase1[o] = data[i]
-    
+
     phase2 = [0] * 8
     for i in range(8):
         phase2[i] = phase1[i] ^ key[i]
-    
+
     phase3 = [0] * 8
     for i in range(8):
         phase3[i] = ( (phase2[i] >> 3) | (phase2[ (i-1+8)%8 ] << 5) ) & 0xff
-    
+
     ctmp = [0] * 8
     for i in range(8):
         ctmp[i] = ( (cstate[i] >> 4) | (cstate[i]<<4) ) & 0xff
-    
+
     out = [0] * 8
     for i in range(8):
         out[i] = (0x100 + phase3[i] - ctmp[i]) & 0xff
-    
+
     return out
 
 def hd(d):
@@ -38,12 +38,12 @@ def now():
     return int(time.time())
 
 if __name__ == "__main__":
-    key = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]    
+    key = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
     fp = open(sys.argv[1], "a+b",  0)
     HIDIOCSFEATURE_9 = 0xC0094806
     set_report = "\x00" + "".join(chr(e) for e in key)
     fcntl.ioctl(fp, HIDIOCSFEATURE_9, set_report)
-    
+
     #parser = argparse.ArgumentParser(description='Plot CO2 and TEMP')
     #parser.add_argument('-d', action="store_true", default=False, help="start as deamon")
     #args = parser.parse_args()
@@ -57,7 +57,8 @@ if __name__ == "__main__":
 
     data_encrypted_print = False
     while True:
-        data = list(ord(e) for e in fp.read(8))
+        byte_data = fp.read(8)
+        data = list(byte_data)
         if data[4] == 0x0d and (sum(data[:3]) & 0xff) == data[3]:
             decrypted = data
             if data_encrypted_print == False:
@@ -66,7 +67,7 @@ if __name__ == "__main__":
         else:
             decrypted = decrypt(key, data)
         if decrypted[4] != 0x0d or (sum(decrypted[:3]) & 0xff) != decrypted[3]:
-            print hd(data), " => ", hd(decrypted),  "Checksum error"
+            print(hd(data), " => ", hd(decrypted),  "Checksum error")
         else:
             op = decrypted[0]
             val = decrypted[1] << 8 | decrypted[2]
@@ -78,7 +79,7 @@ if __name__ == "__main__":
 
                 sys.stdout.write("CO2: %4i TMP: %3.1f    \r" % (co2, tmp))
                 sys.stdout.flush()
-            
+
                 if now() - stamp > 60:
-                    print ">>> sending dataset CO2: %4i TMP: %3.1f .." % (co2, tmp)
+                    print(">>> sending dataset CO2: %4i TMP: %3.1f .." % (co2, tmp))
                     stamp = now()
